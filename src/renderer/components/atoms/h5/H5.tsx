27856d21449a867166bf6b42c2cc08f734e776a7
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 16:23:47 +0300

    Create H5 atom

---
+++ src/renderer/components/atoms/h5/H5.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './H5.module.scss';
+
+interface H5Props {
+  children: React.ReactNode | string;
+  className?: string;
+}
+
+const H5 = ({ children, className }: H5Props) => {
+  return <h5 className={className}>{children}</h5>;
+};
+
+H5.defaultProps = {
+  className: 'title',
+};
+
+export default H5;
