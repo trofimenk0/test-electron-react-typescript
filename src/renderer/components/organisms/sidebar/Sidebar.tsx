b4dfcbb7c6dd4176b2b2a6391f5c363c730c2be3
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 12:27:51 +0300

    Add Menu molecule to Sidebar organism

--- src/renderer/components/organisms/sidebar/Sidebar.tsx
+++ src/renderer/components/organisms/sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from 'renderer/components/atoms/image/Image';
+import Menu from 'renderer/components/molecules/menu/Menu';
 import styles from './Sidebar.module.scss';
+import Logo from '../../../assets/images/Logo.svg';
 
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <Image height="100px" />
+      <Image height="auto" src={Logo} width="100%" />
+      <Menu />
     </aside>
   );
 };
