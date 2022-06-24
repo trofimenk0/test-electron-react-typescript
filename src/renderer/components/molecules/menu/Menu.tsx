d27a3774c8485ffcc5b7597b7903a1ae8419bd38
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 13:14:12 +0300

    Add link to home page

--- src/renderer/components/molecules/menu/Menu.tsx
+++ src/renderer/components/molecules/menu/Menu.tsx
@@ -5,14 +5,15 @@ import styles from './Menu.module.scss';
 const Menu = () => {
   return (
     <nav className={styles.menu}>
+      <NavLink to="/">Dashboard</NavLink>
       <NavLink to="/clients">Clients</NavLink>
       <NavLink to="/teachers">Teachers</NavLink>
       <NavLink to="/courses">Courses</NavLink>
       <NavLink to="/payments">Payments</NavLink>
       <NavLink to="/invoices">Invoices</NavLink>
-      <NavLink to="/reports">Reports</NavLink>
+      {/* <NavLink to="/reports">Reports</NavLink>
       <NavLink to="/import-export">Import / Export</NavLink>
-      <NavLink to="/settings">Settings</NavLink>
+      <NavLink to="/settings">Settings</NavLink> */}
     </nav>
   );
 };
