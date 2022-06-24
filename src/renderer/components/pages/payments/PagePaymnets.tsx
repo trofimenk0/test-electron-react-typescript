4b732d35c8c255de58a5ed8ccb1e83f59ca442cd
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 13:09:12 +0300

    Create PagePayments page

---
+++ src/renderer/components/pages/payments/PagePaymnets.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TemplateDefault from 'renderer/components/templates/default/TemplateDefault';
+import styles from './PagePaymnets.module.scss';
+
+const PagePaymnets = () => {
+  return (
+    <div className={styles.PagePaymnets}>
+      <TemplateDefault>Teachers page</TemplateDefault>
+    </div>
+  );
+};
+
+export default PagePaymnets;
