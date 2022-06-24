9e579c6509638cf700200b2791cbd88e1cd9654a
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 14:07:46 +0300

    Add icons to menu links

--- src/renderer/components/molecules/menu/Menu.tsx
+++ src/renderer/components/molecules/menu/Menu.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import NavLink from 'renderer/components/atoms/nav-link/NavLink';
+import Image from 'renderer/components/atoms/image/Image';
 import styles from './Menu.module.scss';
+
+import IconHome from '../../../assets/icons/IconHome.svg';
+import IconClients from '../../../assets/icons/IconClients.svg';
+import IconTeachers from '../../../assets/icons/IconTeachers.svg';
+import IconCourses from '../../../assets/icons/IconCourses.svg';
+import IconPayments from '../../../assets/icons/IconPayments.svg';
+import IconInvoices from '../../../assets/icons/IconInvoices.svg';
 
 const Menu = () => {
   return (
     <nav className={styles.menu}>
-      <NavLink to="/">Dashboard</NavLink>
-      <NavLink to="/clients">Clients</NavLink>
-      <NavLink to="/teachers">Teachers</NavLink>
-      <NavLink to="/courses">Courses</NavLink>
-      <NavLink to="/payments">Payments</NavLink>
-      <NavLink to="/invoices">Invoices</NavLink>
+      <NavLink to="/">
+        <Image src={IconHome} className="menuLinkIcon" />
+        Dashboard
+      </NavLink>
+      <NavLink to="/clients">
+        <Image src={IconClients} className="menuLinkIcon" />
+        Clients
+      </NavLink>
+      <NavLink to="/teachers">
+        <Image src={IconTeachers} className="menuLinkIcon" />
+        Teachers
+      </NavLink>
+      <NavLink to="/courses">
+        <Image src={IconCourses} className="menuLinkIcon" />
+        Courses
+      </NavLink>
+      <NavLink to="/payments">
+        <Image src={IconPayments} className="menuLinkIcon" />
+        Payments
+      </NavLink>
+      <NavLink to="/invoices">
+        <Image src={IconInvoices} className="menuLinkIcon" />
+        Invoices
+      </NavLink>
       {/* <NavLink to="/reports">Reports</NavLink>
       <NavLink to="/import-export">Import / Export</NavLink>
       <NavLink to="/settings">Settings</NavLink> */}
