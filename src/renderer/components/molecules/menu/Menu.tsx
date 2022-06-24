514045c19ea879f21dbf34b8f96faf68caf70e0f
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 13:05:11 +0300

    Add links to menu

--- src/renderer/components/molecules/menu/Menu.tsx
+++ src/renderer/components/molecules/menu/Menu.tsx
@@ -6,13 +6,13 @@ const Menu = () => {
   return (
     <nav className={styles.menu}>
       <NavLink to="/clients">Clients</NavLink>
-      <NavLink to="/">Teachers</NavLink>
-      <NavLink to="/">Courses</NavLink>
-      <NavLink to="/">Payments</NavLink>
-      <NavLink to="/">Invoices</NavLink>
-      <NavLink to="/">Reports</NavLink>
-      <NavLink to="/">Import / Export</NavLink>
-      <NavLink to="/">Settings</NavLink>
+      <NavLink to="/teachers">Teachers</NavLink>
+      <NavLink to="/courses">Courses</NavLink>
+      <NavLink to="/payments">Payments</NavLink>
+      <NavLink to="/invoices">Invoices</NavLink>
+      <NavLink to="/reports">Reports</NavLink>
+      <NavLink to="/import-export">Import / Export</NavLink>
+      <NavLink to="/settings">Settings</NavLink>
     </nav>
   );
 };
