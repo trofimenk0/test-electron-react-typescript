0342e8c81497b996fe87b430344ea9f4ff95d031
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 13:13:56 +0300

    Add routes for created pages

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,12 +4,20 @@ import './App.css';
 
 import PageHome from './components/pages/home/PageHome';
 import PageClients from './components/pages/clients/PageClients';
+import PageTeachers from './components/pages/teachers/PageTeachers';
+import PageCourses from './components/pages/courses/PageCourses';
+import PagePaymnets from './components/pages/payments/PagePaymnets';
+import PageInvoices from './components/pages/invoices/PageInvoices';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<PageHome />} />
       <Route path="/clients" element={<PageClients />} />
+      <Route path="/teachers" element={<PageTeachers />} />
+      <Route path="/courses" element={<PageCourses />} />
+      <Route path="/payments" element={<PagePaymnets />} />
+      <Route path="/invoices" element={<PageInvoices />} />
     </Routes>
   );
 };
