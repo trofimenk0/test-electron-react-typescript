d8681dde7b9cd1273a90494e8d3c715b8e10bedc
trofimenk0 <trofimenkodpwork@gmail.com>
2022-06-24 12:50:57 +0300

    Add NavLink to Logo in Sidebar

--- src/renderer/components/organisms/sidebar/Sidebar.tsx
+++ src/renderer/components/organisms/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavLink from 'renderer/components/atoms/nav-link/NavLink';
 import Image from 'renderer/components/atoms/image/Image';
 import Menu from 'renderer/components/molecules/menu/Menu';
 import styles from './Sidebar.module.scss';
@@ -7,7 +8,9 @@ import Logo from '../../../assets/images/Logo.svg';
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <Image height="auto" src={Logo} width="100%" />
+      <NavLink to="/">
+        <Image height="auto" src={Logo} width="100%" />
+      </NavLink>
       <Menu />
     </aside>
   );
